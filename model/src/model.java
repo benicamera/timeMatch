313dc2046f560d336a931eba1c149dafecd3ea7a
Bernhard <Bernhard@DESKTOP-UO40BHP.fritz.box>
2020-06-20 10:14:32 +0200

    arrays und Variablen

---
+++ model/src/model.java
@@ -0,0 +1,30 @@
+
+public class model {
+	private int x1 = 1;
+	private int x2 = 0;
+	private int x3 = 1;
+	private int x4 = 1;
+	private int x5 = 1;
+	private int x6 = 1;
+	private int x7 = 0;
+	private int x8 = 1;
+	private int x9 = 0;
+	private int x10 = 1;
+	private int x11 = 0;
+	private int x12 = 1;
+	private int z1 = 0;
+	private int z2 = 1;
+	private int z3 = 1;
+	private int z4 = 1;
+	private int z5 = 1;
+	private int z6 = 1;
+	private int z7 = 0;
+	private int z8 = 1;
+	private int z9 = 0;
+	private int z10 = 1;
+	private int z11 = 0;
+	private int z12 = 1;
+	//die Variablen sind nur Beispielwerte. Später geben die nutzer diese manuell ein.
+int [] person1 = {x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12};
+int [] person2 = {z1,z2,z3,z4,z5,z6,z7,z8,z9,z10,z11,z12};
+}
