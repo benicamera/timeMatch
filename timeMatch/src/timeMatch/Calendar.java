26f0622b43407ac2d6eeb9a605231534de01077e
gschwibe <gschwibe@e2r85pc102.linuxmuster-net.lokal>
2020-06-25 13:01:39 +0200

    veränderung der arrays

--- timeMatch/src/timeMatch/Calendar.java
+++ timeMatch/src/timeMatch/Calendar.java
@@ -22,28 +22,21 @@ public class Calendar {
 	private int x10 = 1;
 	private int x11 = 1;
 	private int x12 = 1;
-	private int z1 = 1;
-	private int z2 = 1;
-	private int z3 = 0;
-	private int z4 = 1;
-	private int z5 = 1;
-	private int z6 = 0;
-	private int z7 = 1;
-	private int z8 = 1;
-	private int z9 = 0;
-	private int z10 = 1;
-	private int z11 = 0;
-	private int z12 = 1;
 
-	int [] person1 = {x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12};
-	int [] person2 = {z1,z2,z3,z4,z5,z6,z7,z8,z9,z10,z11,z12};
+	int [] time = {x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12};
 	
 	public Calendar() {
 		
 
 		
 	}
-	
+	public int getTime(int i) {
+		return time [i];
+	}
+	public void createCalandar() {
+		
+		
+	}
 	public void toTest(/* insert Parameters*/) {
 
 	}
